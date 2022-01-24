b54a0afdad102c814f49206ec8120393df38b5e3
joshua360x <jkw5499@psu.edu>
2022-01-24 18:09:29 -0500

    first react school commit

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import backgroundImg from '../background.png'
 import './Main.css'
+import ZodiacCard from '../ZodiacCard/ZodiacCard'
 
-export default function Main() {
+export default function Main({ zodiac }) {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
       {/* import the ZodiacCard component */}
       {/* get the zodiac array from props */}
+      { zodiac.map((zodiac, i) => 
+        <ZodiacCard key={i} {...zodiac} />) }
       {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
       {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
     </main>
