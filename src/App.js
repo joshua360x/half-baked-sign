db6d59174a9f327517a67dae171a95a98f1946d8
joshua360x <jkw5499@psu.edu>
2022-02-11 07:30:16 -0500

    tried to refractor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import './App.css'
-import Header from './Header/Header'
-import Footer from './Footer/Footer' 
-import Main from './Main/Main'
-{/* import the zodiac array */}
-import { zodiac } from './data'
+import "./App.css";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Main from "./Main/Main";
+import React from "react";
+{
+  /* import the zodiac array */
+}
+import { zodiac } from "./data";
+// import { render } from "react-dom";
 
-const name = 'The Young One'
-const currentYear = new Date().getFullYear()
+// const name = "The Young One";
+// const currentYear = new Date().getFullYear();
 
-function App() {
-  return (
-    <div className="App">
-      {/* import and use the Header. Main, and Footer components here */}
-      {/* the Header component needs a string passed in as the `name` prop */}
-      <Header name={name}></Header>
-      {/* the Footer component needs a year passed in as the `year` prop */}
-      <Footer year={currentYear}></Footer>
-      {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
-      <Main zodiac={zodiac}/>
-    </div>
-  )
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* import and use the Header. Main, and Footer components here */}
+//       {/* the Header component needs a string passed in as the `name` prop */}
+//       <Header name={name}></Header>
+//       {/* the Footer component needs a year passed in as the `year` prop */}
+//       <Footer year={currentYear}></Footer>
+//       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+//       <Main zodiac={zodiac} />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    const name = 'The Young One'
+    const currentYear = new Date().getFullYear()
+    return (
+      <div className="App">
+        {/* import and use the Header. Main, and Footer components here */}
+        {/* the Header component needs a string passed in as the `name` prop */}
+        <Header name={name}></Header>
+        {/* the Footer component needs a year passed in as the `year` prop */}
+        <Footer year={currentYear}></Footer>
+        {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+        <Main zodiac={zodiac} />
+      </div>
+    )
+  }
 }
 
 export default App
